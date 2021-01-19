0bef2174900cb3e74fc975fcadc27b6afdf174fb
eanisimov <egoranisimov@gmail.com>
2021-01-19 22:41:13 +0300

    v2.0
    Код оптимизирован

--- CatPethouse.py
+++ CatPethouse.py
@@ -1,7 +1,6 @@
 from ClassCat import Cat
 
-baron = Cat('Барон', 'Мальчик', 2)
-sam = Cat('Сэм', 'Мальчик', 2)
+cats = [Cat('Барон', 'Мальчик', 2), Cat('Сэм', 'Мальчик', 2)]
 
-print(baron.get_name(), baron.get_gender(), baron.get_age(), sep=', ')
-print(sam.get_name(), sam.get_gender(), sam.get_age(), sep=', ')
+for i in cats:
+    print(i.get_name(), i.get_gender(), i.get_age(), sep=', ')
